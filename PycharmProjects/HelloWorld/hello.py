f77e5dd21ac2cf841a3b3f695bd183b552d27f44
Sruthivarma128 <sruthi.nallapu1@gmail.com>
2020-01-09 23:35:12 -0500

    commiting hello.py

---
+++ PycharmProjects/HelloWorld/hello.py
@@ -0,0 +1,13 @@
+# Python Strings - single line comments
+"""Multiple line comments
+Python Strings"""
+a="Hello World"
+b='Hello World'
+x="Hello's World"
+y='Hello\'s \\World'
+z="Hello\"s \World"
+print(a)
+print(b)
+print(x)
+print(y)
+print(z)
