bd2551de22c0b5af60befe5136d189668663e0bf
Sruthivarma128 <sruthi.nallapu1@gmail.com>
2020-01-15 18:03:45 -0500

    Commiting Iterators

---
+++ PycharmProjects/Inheritance/Iterators.py
@@ -0,0 +1,17 @@
+class ListIterator:
+    def __init__(self, list):
+        self.__list = list
+        self.__index = -1
+    def __iter__(self):
+        return self
+    def __next__(self):
+        self.__index += 1
+        if self.__index >= len(self.__list):
+            raise StopIteration
+        return self.__list[self.__index]
+a = [1,2,3,4,5,6]
+myList = ListIterator(a)
+it = iter(myList)
+
+for i in a:
+    print(i)
