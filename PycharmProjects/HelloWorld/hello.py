17f32cde7c7c4c11003abee580d2cb5954b3d3c6
Sruthivarma128 <sruthi.nallapu1@gmail.com>
2020-01-18 04:14:10 -0500

    modified hello.py

--- PycharmProjects/HelloWorld/hello.py
+++ PycharmProjects/HelloWorld/hello.py
@@ -2,13 +2,15 @@
 #first Hello World program
 """Multiple line comments
 Python Strings"""
-a="Hello World"
-b='Hello World'
+a="Testing the integration of Github and Jenkins"
+b="Hello World"
+c='Hello World'
 x="Hello's World"
 y='Hello\'s \\World'
 z="Hello\"s \World"
 print(a)
 print(b)
+print(c)
 print(x)
 print(y)
 print(z)
